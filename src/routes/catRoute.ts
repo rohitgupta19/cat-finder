b3bedc3e72bb899b30be7e2935a3a62bd0a66e04
Rohit Gupta <gupta.rohit19@gmail.com>
2022-11-06 23:57:41 +1100

    updated files

--- src/routes/catRoute.ts
+++ src/routes/catRoute.ts
@@ -3,18 +3,18 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-    res.json({
-      success: true,
-      message: 'Cat List',
-    });
+  res.json({
+    success: true,
+    message: 'Cat List'
+  });
 });
 
 router.post('/list', (req: Request, res: Response) => {
-    console.log('req.body', req.body);
-    res.json({
-      success: true,
-      message: 'Cat List',
-    });
+  console.log('req.body', req.body);
+  res.json({
+    success: true,
+    message: 'Cat List'
+  });
 });
 
 export default router;
