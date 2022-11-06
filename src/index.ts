673b576a492b44a226949ac0ec976c8dfaafdcdb
Rohit Gupta <88296389+rohitbsgupta@users.noreply.github.com>
2022-11-06 22:56:40 +1100

    added new files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT;
+
+const allowedOrigins = [''];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('This is a test web page!');
+});
+
+console.log('port', port);
+app.listen(port, () => {
+  console.log(`The application is listening on port ${port}!`);
+});
